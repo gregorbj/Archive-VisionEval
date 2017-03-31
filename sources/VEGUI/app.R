f4a2ffe7133438822a40bcd4b9a6749f680188a9
Peter Andrews <Peter.Andrews@rsginc.com>
2017-03-31 16:14:43 -0400

    runApp expects it be called app.R

--- sources/VEGUI/app.R
+++ sources/VEGUI/app.R
@@ -5,6 +5,10 @@ library(visioneval)
 library(DT)
 library(shinyBS)
 
+if (interactive()) {
+  options(shiny.reactlog = TRUE)
+}
+
 # Define UI for application
 ui <- fluidPage(
   useShinyjs(),
@@ -36,80 +40,63 @@ ui <- fluidPage(
 
     mainPanel(
       bsAlert("parseProblems"),
-      h3("Script Name"),
-      verbatimTextOutput('scriptName', TRUE),
-      h3("Script Output"),
-      verbatimTextOutput('scriptOutput', TRUE),
-      h3("Datastore List"),
-      verbatimTextOutput('datastorelist', TRUE),
-      DT::dataTableOutput('parseModelScriptTable')
+      conditionalPanel(
+        condition =  "true", #"input.file != null",
 
-    )
-
-  )
-
+        h3("Script Name"),
+        verbatimTextOutput('scriptName', TRUE),
+        h3("Script Output"),
+        verbatimTextOutput('scriptOutput', TRUE),
+        h3("Datastore List"),
+        verbatimTextOutput('datastorelist', TRUE)
+        #DT::dataTableOutput('parseModelScriptTable')
+      ) #end conditionalPanel
+    ) #end mainPanel
+  ) #end sidebarLayout
 ) #end ui
 
+
 server <- function(input, output, session) {
-  shinyFileChoose(
+
+    shinyFileChoose(
     input = input,
     id = 'file',
-    roots = c(wd = '.'),
     session = session,
+    roots = getVolumes(),
     filetypes = c('R')
   )
 
   getScriptInfo <- reactive({
+    req(input$file)
     scriptInfo <- list()
     inFile = parseFilePaths(roots = getVolumes(''), input$file)
-    scriptInfo$datapath <- normalizePath(as.character(inFile$datapath))
+    scriptInfo$datapath <-
+      normalizePath(as.character(inFile$datapath))
     scriptInfo$fileDirectory <- dirname(scriptInfo$datapath)
     scriptInfo$fileBase <- basename(scriptInfo$datapath)
     scriptInfo$parsedModelScript <- parse(scriptInfo$datapath)
-    if (sum(grepl("^initializeModel", scriptInfo$parsedModelScript)) != 1) {
+
+    callsToInitialize <- grepl("^initializeModel", scriptInfo$parsedModelScript)
+    if (sum(callsToInitialize) != 1) {
       createAlert(
         session = session,
         anchorId = "parseProblems",
         title = "Invalid initializeModel",
-        content = paste0(datapath, "should have one call to initializeModel")
+        content = paste0(datapath, " should have one call to initializeModel")
       )
       return()
     } else {
-      initializeExpressions <-
-        parsedModelScript[grepl("^initializeModel", parsedModelScript)]
-      script$paramDir <- initializeExpressions$ParamDir
-      # initializeModel(
-      #   ParamDir = "defs",
-      #   RunParamFile = "run_parameters.json",
-      #   GeoFile = "geo.csv",
-      #   ModelParamFile = "model_parameters.json",
-      #   LoadDatastore = NULL,
-      #   IgnoreDatastore = FALSE,
-      #   SaveDatastore = TRUE
-      # )
     }
     return(scriptInfo)
   }) #end reactive
 
-  output$parseModelScriptTable = DT::renderDataTable(getScriptInfo()$parsedModelScript)
+  #output$parseModelScriptTable = DT::renderDataTable(getScriptInfo()$parsedModelScript)
 
   output$scriptName = renderPrint({
     getScriptInfo()$datapath
   })
 
-  output$scriptName = renderPrint({
-    getScriptInfo()$datapath
-  })
-
-  getScriptOutput <- reactive({
-    return(capture.output(source(getScriptInfo()$datapath)))
-  })
-
-  output$scriptOutput = renderPrint( {
-    getScriptOutput()
-  } )
-
-  observeEvent(input$resetScriptOutput, {
+   observeEvent(input$resetScriptOutput, {
     reset("scriptOutput")
   })
 
@@ -118,15 +105,14 @@ server <- function(input, output, session) {
   })
 
   #run model on click
-  observeEvent(input$runmodel, {
+  observeEvent(input$runModel, {
     reset("scriptOutput")
     reset("datastorelist")
-    #run model
-    setwd(script$fileDirectory)
+    scriptInfo <- getScriptInfo()
+    setwd(scriptInfo$fileDirectory)
     output$scriptOutput = renderPrint({
-      capture.output(source(g$scriptName()))
+      capture.output(source(scriptInfo$datapath))
     })
-
     #read resulting datastore
     if (file.exists("ModelState.Rda")) {
       datastorePrint = capture.output(getModelState("Datastore"))
@@ -143,4 +129,4 @@ server <- function(input, output, session) {
 
 } #end server
 
-shinyApp(ui, server)
+app <- shinyApp(ui, server)
