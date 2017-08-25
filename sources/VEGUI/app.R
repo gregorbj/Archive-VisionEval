d3251e88a736f59640800deca8a89ad65f23d2df
Aditya Gore <aditya.gore@rsginc.com>
2017-08-25 15:11:33 -0400

    Fixes #127 Added custom functions (base)file.info() and (shinyFiles)fileGetter to make GUI compatible with R v3.4.1

--- sources/VEGUI/app.R
+++ sources/VEGUI/app.R
@@ -152,7 +152,7 @@ navlistPanel(
     title = "Settings",
     value = TAB_SETTINGS,
     h3("Model parameters"),
-    #shinyAce does not support setting height by lines and the updateAceEditor does not 
+    #shinyAce does not support setting height by lines and the updateAceEditor does not
     #have a height parameter so not sure what to do...
     #https://github.com/trestletech/shinyAce/issues/4
     shinyAce::aceEditor(MODEL_PARAMETERS_FILE, height = (16 * 10), mode = "json"),
@@ -234,6 +234,90 @@ DEFAULT_POLL_INTERVAL <- 1500 #milliseconds
 
 
 server <- function(input, output, session) {
+  ########################################
+  ### shinyFiles is incompatible with R v3.4.1. Two functions are overridden to make it compatible
+  ### with R v3.4.1. The definitions of
+  ### file.info() (base package) and fileGetter() (shinyFiles package) are written below
+  ### Only override the functions if running R >= v3.4
+  #########################################
+  if(R.version$minor >= 4){
+    fileGetter <-  function (roots, restrictions, filetypes, hidden = FALSE)
+    {
+      if (missing(filetypes))
+        filetypes <- NULL
+      if (missing(restrictions))
+        restrictions <- NULL
+      function(dir, root) {#browser()
+        currentRoots <- if (class(roots) == "function")
+          roots()
+        else roots
+        if (is.null(names(currentRoots)))
+          stop("Roots must be a named vector or a function returning one")
+        if (missing(root))
+          root <- names(currentRoots)[1]
+        fulldir <- file.path(currentRoots[root], dir)
+        writable <- as.logical(file.access(fulldir, 2) == 0)
+        files <- list.files(fulldir, all.files = hidden, full.names = TRUE,
+                            no.. = TRUE)
+        files <- gsub(pattern = "//*", "/", files, perl = TRUE)
+        if (!is.null(restrictions) && length(files) != 0) {
+          if (length(files) == 1) {
+            keep <- !any(sapply(restrictions, function(x) {
+              grepl(x, files, fixed = T)
+            }))
+          }
+          else {
+            keep <- !apply(sapply(restrictions, function(x) {
+              grepl(x, files, fixed = T)
+            }), 1, any)
+          }
+          files <- files[keep]
+        }
+        fileInfo <- (file.info(files))
+        fileInfo$filename <- basename(files)
+        fileInfo$extension <- tolower(tools::file_ext(files))
+        validIndex <- which(!is.na(fileInfo$mtime))
+        fileInfo <- fileInfo[validIndex,]
+        fileInfo$mtime <- format(fileInfo$mtime, format = "%Y-%m-%d-%H-%M")
+        fileInfo$ctime <- format(fileInfo$ctime, format = "%Y-%m-%d-%H-%M")
+        fileInfo$atime <- format(fileInfo$atime, format = "%Y-%m-%d-%H-%M")
+        if (!is.null(filetypes)) {
+          matchedFiles <- tolower(fileInfo$extension) %in%
+            tolower(filetypes) & fileInfo$extension != ""
+          fileInfo$isdir[matchedFiles] <- FALSE
+          fileInfo <- fileInfo[matchedFiles | fileInfo$isdir,
+                               ]
+        }
+        rownames(fileInfo) <- NULL
+        breadcrumps <- strsplit(dir, .Platform$file.sep)[[1]]
+        list(files = fileInfo[, c("filename", "extension", "isdir",
+                                  "size", "mtime", "ctime", "atime")], writable = writable,
+             exist = file.exists(fulldir), breadcrumps = I(c("",
+                                                             breadcrumps[breadcrumps != ""])), roots = I(names(currentRoots)),
+             root = root)
+      }
+    }
+    unlockBinding("fileGetter", getNamespace("shinyFiles"))
+    assign("fileGetter",fileGetter,getNamespace("shinyFiles"))
+
+    file.info <- function (..., extra_cols = TRUE)
+    {
+      suppressWarnings(res <- .Internal(file.info(fn <- c(...), extra_cols)))
+      res$mtime <- .POSIXct(res$mtime)
+      res$ctime <- .POSIXct(res$ctime)
+      res$atime <- .POSIXct(res$atime)
+      class(res) <- "data.frame"
+      attr(res, "row.names") <- fn
+      res
+    }
+    unlockBinding("file.info", getNamespace("base"))
+    assign("file.info",file.info,getNamespace("base"))
+}
+  ##########################
+  #########################
+  ### End of custom definitions for R >v3.4
+  ##########################
+  ########################
   otherReactiveValues <-
     reactiveValues() #WARNING- DON'T USE VARIABLES TO INITIALIZE LIST KEYS - the variable name will be used, not the value
 
@@ -308,7 +392,7 @@ server <- function(input, output, session) {
   }
 
   #http://stackoverflow.com/questions/38064038/reading-an-rdata-file-into-shiny-application
-  # This function, borrowed from http://www.r-bloggers.com/safe-loading-of-rdata-files/, 
+  # This function, borrowed from http://www.r-bloggers.com/safe-loading-of-rdata-files/,
   #load the Rdata into a new environment to avoid side effects
   LoadToEnvironment <-
     function(filePath, env = new.env(parent = emptyenv())) {
@@ -574,22 +658,22 @@ server <- function(input, output, session) {
       scriptInfo$fileDirectory <- dirname(scriptInfo$datapath)
       scriptInfo$fileBase <- basename(scriptInfo$datapath)
       debugConsole(paste("getScriptInfo:", scriptInfo$datapath))
-      
+
       #call the first few methods so can find out log file value and get the ModelState_ls global
       setwd(scriptInfo$fileDirectory)
       visioneval::initModelStateFile()
       visioneval::initLog()
       visioneval::writeLog("VE_GUI called visioneval::initModelStateFile() and visioneval::initLog()")
-      
+
       #Fome now on we will get the current ModelState by reading the object stored on disk
       reactiveFilePaths[[MODEL_STATE_FILE]] <<-
         file.path(scriptInfo$fileDirectory, "ModelState.Rda")
-      
-      reactiveFilePaths[[VE_LOG]] <<- 
+
+      reactiveFilePaths[[VE_LOG]] <<-
         file.path(scriptInfo$fileDirectory, getModelState()$LogFile)
-      reactiveFilePaths[[DATASTORE]] <<- 
+      reactiveFilePaths[[DATASTORE]] <<-
         file.path(scriptInfo$fileDirectory, getModelState()$DatastoreName)
-      
+
       defsDirectory <- file.path(scriptInfo$fileDirectory, "defs")
 
       reactiveFilePaths[[MODEL_PARAMETERS_FILE]] <<-
