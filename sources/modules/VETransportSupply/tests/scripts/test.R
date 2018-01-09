0f6eef0f033ad79a51a6a5030f76502bf33e4beb
Aditya Gore <aditya.gore@rsginc.com>
2018-01-09 11:46:56 -0500

    Added * scripts that implement the accessibility model of RPAT; and * inputs and test script to ensure that the module runs.

--- sources/modules/VETransportSupply/tests/scripts/test.R
+++ sources/modules/VETransportSupply/tests/scripts/test.R
@@ -1,6 +1,10 @@
 library(rhdf5)
 library(filesstrings)
 
+##################################################
+## Section 1. Tests for modules used in VERSPM
+##################################################
+
 #Load datastore from VELandUse package
 file.copy("../VELandUse/tests/Datastore.tar", "tests/Datastore.tar")
 setwd("tests")
@@ -30,4 +34,60 @@ testModule(
 setwd("tests")
 tar("Datastore.tar", "Datastore")
 dir.remove("Datastore")
+setwd("..")
+
+
+##################################################
+## Section 2. Tests for modules used in VERPAT
+##################################################
+
+# Set the working directory to tests folder and load the output from
+# previous module runs
+setwd("tests")
+# Copy and save the results from previous module tests
+zip("ModelState.zip","ModelState.Rda")
+file.remove("ModelState.Rda")
+tar("defs.tar","defs")
+tar("inputs.tar","inputs")
+dir.remove("defs")
+dir.remove("inputs")
+untar("Datastore_VERPAT.tar")
+unzip("ModelState_VERPAT.zip")
+untar("inputs_VERPAT.tar")
+setwd("..")
+
+#Test CreateBaseAccessibility module
+source("R/CreateBaseAccessibility.R")
+testModule(
+  ModuleName = "CreateBaseAccessibility",
+  LoadDatastore = TRUE,
+  SaveDatastore = TRUE,
+  DoRun = TRUE,
+  RunFor = "BaseYear"
+)
+
+#Test CreateFutureAccessibility module
+source("R/CreateFutureAccessibility.R")
+testModule(
+  ModuleName = "CreateFutureAccessibility",
+  LoadDatastore = TRUE,
+  SaveDatastore = TRUE,
+  DoRun = TRUE,
+  RunFor = "NotBaseYear"
+)
+
+#Finish up
+setwd("tests")
+tar("Datastore_VERPAT.tar", c("Datastore", "defs"))
+dir.remove("Datastore")
+dir.remove("defs")
+dir.remove("inputs")
+zip("ModelState_VERPAT.zip","ModelState.Rda")
+file.remove("ModelState.Rda")
+unzip("ModelState.zip")
+file.remove("ModelState.zip")
+untar("defs.tar")
+file.remove("defs.tar")
+untar("inputs.tar")
+file.remove("inputs.tar")
 setwd("..")
