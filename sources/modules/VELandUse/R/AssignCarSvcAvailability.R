e6a33331a276517809a6bdc4f6f2209d5b6a7615
gregorbj <gregorbj@gmail.com>
2018-04-10 19:43:26 -0700

    Change module name from AssignCarSvcCharacteristics to AssignCarSvcAvailability

--- sources/modules/VELandUse/R/AssignCarSvcAvailability.R
+++ sources/modules/VELandUse/R/AssignCarSvcAvailability.R
@@ -1,25 +1,22 @@
-#=============================
-#AssignCarSvcCharacteristics.R
-#=============================
-#This module reads in and assigns car service characteristics to Bzones. Car
+#==========================
+#AssignCarSvcAvailability.R
+#==========================
+#This module reads in and assigns car service availability in Bzones. Car
 #services include taxis (with human drivers or under automated control) and car
 #sharing services (e.g. Car-To-Go, Zipcar), and future automated taxi services.
 #A user input file identifies which Bzones have high or low levels of service. A
 #high car service level is one that has access times that are competitive with
 #private car use. This means that the time it takes for a taxi service to pick
 #up a passenger or that time it takes to get to a car share car is not much
-#longer than the time to get to and from a vehicle owned by a household. High
-#level of car service is considered to increase household car availability and
-#affects the amount of household DVMT. Low level car service does not have
-#competitive access time and is not considered as increasing household car
-#availability. Low level car service characteristics determine what a household
-#living in an area with low level service will pay for their vehicle travel.
-#User inputs also determine car service characteristics at the Azone level. The
-#average access time and the average cost per mile at the two service levels.
-#These values are used to calculate the cost of using car services to determine
-#what households might choose to use a car service instead of owning one or more
-#vehicles, and to determine the relative utility of car services compared to
-#owned cars to determine how car service availability affects household DVMT.
+#longer than the time to get to and from a vehicle owned by a household,
+#especially when considering that a car service may be able to drop off the
+#passengers at the destination, or may have preferential parking, thus avoiding
+#time to park a private vehicle and walk to the destination. High level of car
+#service is considered to increase household car availability and affects the
+#amount of household DVMT. Low level car service does not have competitive
+#access time and is not considered as increasing household car availability. Low
+#level car service characteristics determine what a household living in an area
+#with low level service will pay for their vehicle travel.
 
 
 #=================================
@@ -41,7 +38,7 @@
 
 #Define the data specifications
 #------------------------------
-AssignCarSvcCharacteristicsSpecifications <- list(
+AssignCarSvcAvailabilitySpecifications <- list(
   #Level of geography module is applied at
   RunBy = "Azone",
   #Specify new tables to be created by Inp if any
@@ -114,7 +111,7 @@ AssignCarSvcCharacteristicsSpecifications <- list(
 
 #Save the data specifications list
 #---------------------------------
-#' Specifications list for AssignCarSvcCharacteristics module
+#' Specifications list for AssignCarSvcAvailability module
 #'
 #' A list containing specifications for the AssignCarSvcCharacteristics module.
 #'
@@ -125,9 +122,9 @@ AssignCarSvcCharacteristicsSpecifications <- list(
 #'  \item{Get}{module inputs to be read from the datastore}
 #'  \item{Set}{module outputs to be written to the datastore}
 #' }
-#' @source AssignCarSvcCharacteristics.R script.
-"AssignCarSvcCharacteristicsSpecifications"
-devtools::use_data(AssignCarSvcCharacteristicsSpecifications, overwrite = TRUE)
+#' @source AssignCarSvcAvailability.R script.
+"AssignCarSvcAvailabilitySpecifications"
+devtools::use_data(AssignCarSvcAvailabilitySpecifications, overwrite = TRUE)
 
 
 #=======================================================
@@ -140,7 +137,7 @@ devtools::use_data(AssignCarSvcCharacteristicsSpecifications, overwrite = TRUE)
 #---------------------------------------------------------------------
 #' Main module function to assign car service level to each household.
 #'
-#' \code{AssignCarSvcCharacteristics} assigns the car service level to each
+#' \code{AssignCarSvcAvailability} assigns the car service level to each
 #' household based on the Bzone where the household is located.
 #'
 #' This function assigns the car service level to each household based on the
@@ -152,7 +149,7 @@ devtools::use_data(AssignCarSvcCharacteristicsSpecifications, overwrite = TRUE)
 #' specifications for the module.
 #' @import visioneval stats
 #' @export
-AssignCarSvcCharacteristics <- function(L) {
+AssignCarSvcAvailability <- function(L) {
 
   #Assign car service level to each household
   #------------------------------------------
@@ -178,19 +175,19 @@ AssignCarSvcCharacteristics <- function(L) {
 #module functions
 #-------------------------------------------------------------------------------
 # TestDat_ <- testModule(
-#   ModuleName = "AssignCarSvcCharacteristics",
+#   ModuleName = "AssignCarSvcAvailability",
 #   LoadDatastore = TRUE,
 #   SaveDatastore = TRUE,
 #   DoRun = FALSE
 # )
 # L <- TestDat_$L
-# R <- AssignCarSvcCharacteristics(L)
+# R <- AssignCarSvcAvailability(L)
 
 #Test code to check everything including running the module and checking whether
 #the outputs are consistent with the 'Set' specifications
 #-------------------------------------------------------------------------------
 # TestDat_ <- testModule(
-#   ModuleName = "AssignCarSvcCharacteristics",
+#   ModuleName = "AssignCarSvcAvailability",
 #   LoadDatastore = TRUE,
 #   SaveDatastore = TRUE,
 #   DoRun = TRUE
