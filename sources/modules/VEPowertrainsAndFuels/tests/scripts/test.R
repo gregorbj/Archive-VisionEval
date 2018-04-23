edcc41a373f24237125a0e51920414ddf90a780d
gregorbj <gregorbj@gmail.com>
2018-04-23 09:18:26 -0700

    Make new VEPowertrainsAndFuels package that includes the LoadDefaultValues, Initialize, CalculateCarbonIntensity, and AssignVehiclePowertrain modules from the VEEnergyAndEmissions package

---
+++ sources/modules/VEPowertrainsAndFuels/tests/scripts/test.R
@@ -0,0 +1,46 @@
+library(visioneval)
+library(data.table)
+library(filesstrings)
+
+#Load datastore from VERoadPerformance package
+file.copy("../VEHouseholdTravel/tests/Datastore.tar", "tests/Datastore.tar")
+setwd("tests")
+untar("Datastore.tar")
+file.remove("Datastore.tar")
+setwd("..")
+
+#Load energy and emissions default data
+load("data/PowertrainFuelDefaults_ls.rda")
+
+#Test Initialize module
+source("R/Initialize.R")
+testModule(
+  ModuleName = "Initialize",
+  LoadDatastore = TRUE,
+  SaveDatastore = TRUE,
+  DoRun = TRUE
+)
+
+#Test CalculateCarbonIntensity module
+source("R/CalculateCarbonIntensity.R")
+testModule(
+  ModuleName = "CalculateCarbonIntensity",
+  LoadDatastore = TRUE,
+  SaveDatastore = TRUE,
+  DoRun = TRUE
+)
+
+#Test AssignHhVehiclePowertrain module
+source("R/AssignHhVehiclePowertrain.R")
+testModule(
+  ModuleName = "AssignHhVehiclePowertrain",
+  LoadDatastore = TRUE,
+  SaveDatastore = TRUE,
+  DoRun = TRUE
+)
+
+#Finish up
+setwd("tests")
+tar("Datastore.tar", "Datastore")
+dir.remove("Datastore")
+setwd("..")
