0c6cde2f955130b31fb87b23a7ec96d01cebea02
Aditya Gore <aditya.gore@rsginc.com>
2018-05-08 10:21:29 -0400

    Changed the sequence of package installation to address dependency issues New faster version of install.R script

--- install.R
+++ install.R
@@ -6,24 +6,63 @@
 #set_config(use_proxy(url="proxynew.odot.state.or.us", port=8080))
 #set_config( config( ssl_verifypeer = 0L ) )
 
-# Download and install the required libraries and their dependencies
-install.packages(c("curl","devtools", "roxygen2", "stringr", "knitr", "digest"), dependencies = TRUE)
-install.packages(c("shiny", "shinyjs", "shinyFiles", "data.table", "DT", "shinyBS", "future", "testit", "jsonlite", "shinyAce", "envDocument", "rhandsontable","shinyTree"), dependencies = TRUE)
-devtools::install_github("tdhock/namedCapture")
-source("https://bioconductor.org/biocLite.R")
-biocLite(c("rhdf5","zlibbioc"), suppressUpdates=TRUE)
+#Set repository
+local({currentRepo <- getOption("repos")
+currentRepo["CRAN"] <- "https://cran.cnr.berkeley.edu/"
+options(repos = currentRepo)
+})
 
-#Download and install the required VE framework package
-devtools::install_github("gregorbj/VisionEval/sources/framework/visioneval")
+# Download and install the required libraries and their dependencies
+cat("\nInstalling dependencies\n")
+install.packages(c("curl","devtools", "roxygen2", "stringr", "knitr", "digest"), dependencies = TRUE, quiet=TRUE)
+install.packages(c("shiny", "shinyjs", "shinyFiles", "data.table", "DT", "shinyBS", "future", "testit", "jsonlite", "shinyAce", "envDocument", "rhandsontable","shinyTree"), dependencies = TRUE, quiet=TRUE)
+devtools::install_github("tdhock/namedCapture", quiet=TRUE)
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("rhdf5","zlibbioc"), suppressUpdates=TRUE, quiet=TRUE)
+
+#Download the VE repository
+host <- "https://api.github.com/repos/"
+repo <- "gregorbj/visioneval/"
+ref <- "master" #"develop"
+
+destfile <- tempfile(fileext = paste0(".zip"))
+destdir <- normalizePath(tempdir())
+cat("\nDownloading VE repository\n")
+request <- httr::GET(paste0(host, repo, "zipball/", ref))
+if(httr::status_code(request) >= 400){
+	stop("\nError downloading the repository\n")
+}
+writeBin(httr::content(request, "raw"), destfile)
+unzip(zipfile = destfile, exdir = destdir)
+destdir <- normalizePath(file.path(destdir, grep("visioneval", list.files(destdir), value=TRUE, ignore.case=TRUE)))
+cat("\nFinished downloading VE repository\n")
+cat(paste0("\n","Download directory: ", destdir, "\n"))
+
+# VE framework and modules
+VE_framework <- "visioneval"
+VE_modules <- c(
+	"VE2001NHTS",
+	"VESyntheticFirms",
+	"VESimHouseholds",
+	"VELandUse",
+	"VETransportSupply",
+	"VETransportSupplyUse",
+	"VEHouseholdTravel",
+	"VEHouseholdVehicles",
+	"VEPowertrainsAndFuels",
+	"VETravelPerformance",
+	"VEReports"
+)
+
+#Install the required VE framework package
+cat("\nInstalling VE framework\n")
+devtools::install_local(normalizePath(file.path(destdir, "sources", "framework", VE_framework)))
 
 #Download and install the required VE modules for VERPAT and VERSPM
-devtools::install_github("gregorbj/VisionEval/sources/modules/VESyntheticFirms")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VESimHouseholds")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VELandUse")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VETransportSupply")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VETransportSupplyUse")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VEHouseholdVehicles")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VEHouseholdTravel")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VEPowertrainsAndFuels")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VETravelPerformance")
-devtools::install_github("gregorbj/VisionEval/sources/modules/VEReports")
+for(module in VE_modules){
+	cat(paste("\nInstalling Module:", module,"\n"))
+	devtools::install_local(normalizePath(file.path(destdir, "sources", "modules", module)))
+	if(!module %in% rownames(installed.packages())){
+		stop(paste0(module, " cannot be installed."))
+	}
+}
