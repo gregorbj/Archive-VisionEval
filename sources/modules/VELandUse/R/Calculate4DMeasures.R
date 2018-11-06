302c86fd4a21dfbe9f3c7a15d62bfb5fa586e06a
gregorbj <gregorbj@gmail.com>
2018-11-06 15:08:09 -0800

    Add module documentation and replace devtools::use_data function call with usethis::use_data

--- sources/modules/VELandUse/R/Calculate4DMeasures.R
+++ sources/modules/VELandUse/R/Calculate4DMeasures.R
@@ -1,10 +1,59 @@
 #=====================
 #Calculate4DMeasures.R
 #=====================
-#This module calculates several 4D measures by Bzone including density,
-#diversity (i.e. mixing of land uses), design (i.e. multimodal network design),
-#and destination accessibility.
 
+#<doc>
+#
+## Calculate4DMeasures Module
+#### November 6, 2018
+#
+#This module calculates several *4D* measures by Bzone including density, diversity (i.e. mixing of land uses), transportation network design, and destination accessibility. These measures are the same as or are similar to measures included in the Environmental Protection Agency's (EPA) [Smart Location Database](https://www.epa.gov/smartgrowth/smart-location-database-technical-documentation-and-user-guide).
+#
+### Model Parameter Estimation
+#
+#This module has no parameters. 4D measures are calculated based on Bzone attributes as described in the next section.
+#
+### How the Module Works
+#
+#This module calculates 3 development density measures that are named using the names used in the Smart Location Database (SLD): population density (D1B), employment density (D1C), and activity density (D1D). These density measures are calculated at the Bzone level. The population, employment, and activity (employment + households) values to calculate these measures come from the products of other modules. The area data comes from user inputs of the unprotected area (measured in acres) in urban (i.e. urbanized) and rural (i.e. not urbanized) portions of each Bzone.
+#
+#The module calculates 3 development diversity measures which measure the relative heterogeity of land uses in each Bzone. These too are named according to how the SLD names them. D2A_JPHH is the ratio of jobs to households in each Bzone. D2A_WRKEMP is the ratio of workers living in the zone to jobs located in the zone. D2A_EPHHM is an entropy measure calculated from the amount of activity in 4 categories, 3 employment categories (retail, service, other) measured by the number of jobs in the Bzone, and a household category. Entropy is measured on a scale 0 to 1 with 0 corresponding to the situation where only one activity category (or no activity) is present in the Bzone, and 1 corresponding to the situation where there are equal amounts of all activities in the Bzone. Where 2 or more activity categories are present in the Bzone, the entropy of the Bzone is calculated as follows:
+#
+#  `-sum(R * LogR) / log(NAct)`
+#
+#where:
+#
+#- `R` is a vector of the ratio of activity in each activity category divided by the total of all activity
+#
+#- `LogR` is the natural log of `R` or 0 for activity categories having no activity
+#
+#- `NAct` is the number of activity categories (i.e. 4)
+#
+#The module also calculates a destination accessibility measure (D5) which is the harmonic mean of jobs within 2 miles and population within 5 miles of the Bzone centroid. The calculation uses the simplifying assumption that all jobs and all households in a Bzone are located at the Bzone centroid. The straight line distances between all Bzone centroids are calculated from the latitudes and longitudes of the centroids that are provided by the user. For each Bzone, tabulations are made of the number of jobs located in Bzones whose centroids are within 2 miles and the population located in Bzones whose centroids are within 5 miles of the Bzone centroid. The D5 measure for the Bzone is calculated as follows:
+#
+#  `2 * EmpIn2Mi * PopIn5Mi / (EmpIn2Mi + PopIn5Mi)`
+#
+#where:
+#
+#- `EmpIn2Mi` is the total number of jobs in Bzones whose centroids are located within 2 miles (straight line distance) of the centroid of the subject Bzone.
+#
+#- `PopIn5Mi` is the total population in Bzones whose centroids are located within 5 miles (straight-line distance) of the centroid of the subject Bzone.
+#
+#One transportation network design measures is produced by the module. D3bpo4 is intersection density in terms of pedestrian-oriented intersections having four or more legs per square mile. This is one of the network design measured in the Smart Location database. Users may pivot off of information from the SLD or may compute the measure using GIS. The SLD calculated values using NAVTEQ (now part of Here) network. The SLD users guide defines pedestrian-oriented facilities as follows:
+#
+#- Any arterial or local street having a speed category of 6 (between 21 and 30 mph) where car travel is permitted in both directions.
+#
+#- Any arterial or local street having a speed category of 7 or lower (less than 21 mph).
+#
+#- Any local street having a speed category of 6 (between 21 and 30 mph).
+#
+#- Any pathway or trail on which automobile travel is not permitted (speed category 8).
+#
+#- For all of the above, pedestrians must be permitted on the link.
+#
+#- For all of the above, controlled access highways, tollways, highway ramps, ferries, parking lot roads, tunnels, and facilities having four or more lanes of travel in a single direction (implied eight lanes bi-directional) are excluded.
+#
+#</doc>
 
 
 #=================================
@@ -288,7 +337,7 @@ Calculate4DMeasuresSpecifications <- list(
 #' }
 #' @source Calculate4DMeasures.R script.
 "Calculate4DMeasuresSpecifications"
-devtools::use_data(Calculate4DMeasuresSpecifications, overwrite = TRUE)
+usethis::use_data(Calculate4DMeasuresSpecifications, overwrite = TRUE)
 
 
 #=======================================================
@@ -464,9 +513,13 @@ Calculate4DMeasures <- function(L) {
 }
 
 
-#================================
-#Code to aid development and test
-#================================
+#===============================================================
+#SECTION 4: MODULE DOCUMENTATION AND AUXILLIARY DEVELOPMENT CODE
+#===============================================================
+#Run module automatic documentation
+#----------------------------------
+documentModule("Calculate4DMeasures")
+
 #Test code to check specifications, loading inputs, and whether datastore
 #contains data needed to run module. Return input list (L) to use for developing
 #module functions
