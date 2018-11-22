2e852b52419d1dc1b5d80daa15071a69b0e4f8b8
gregorbj <gregorbj@gmail.com>
2018-11-22 08:55:50 -0800

    Change devtools::use_data function call to usethis::use_data

--- sources/modules/VEHouseholdTravel/R/CalculatePolicyVmt.R
+++ sources/modules/VEHouseholdTravel/R/CalculatePolicyVmt.R
@@ -38,7 +38,7 @@ LtVehOwnModels_ls$NonMetro <- "-0.0864599943963062 * Intercept + 0.1561530952889
 #' }
 #' @source AssignVehicleFeatures.R script.
 "LtVehOwnModels_ls"
-devtools::use_data(LtVehOwnModels_ls, overwrite = TRUE)
+usethis::use_data(LtVehOwnModels_ls, overwrite = TRUE)
 
 # Load the DVMT assignment model
 load("data/DvmtLmModels_ls.rda")
@@ -79,7 +79,7 @@ AveSovPropModels_ls$Parm <- list(
 #' }
 #' @source CalculatePolicyVmt.R script.
 "AveSovPropModels_ls"
-devtools::use_data(AveSovPropModels_ls, overwrite = TRUE)
+usethis::use_data(AveSovPropModels_ls, overwrite = TRUE)
 
 #================================================
 #SECTION 2: DEFINE THE MODULE DATA SPECIFICATIONS
@@ -1224,7 +1224,7 @@ CalculatePolicyVmtSpecifications <- list(
 #' }
 #' @source CalculatePolicyVmt.R script.
 "CalculatePolicyVmtSpecifications"
-devtools::use_data(CalculatePolicyVmtSpecifications, overwrite = TRUE)
+usethis::use_data(CalculatePolicyVmtSpecifications, overwrite = TRUE)
 
 
 #=======================================================
