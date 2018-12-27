3796145bf7155812544aa1d10b02ce23e955b2c7
bstabler <benstabler@yahoo.com>
2018-12-26 17:00:43 -0800

    add updated scenario viewer to gh-pages to publish

---
+++ sources/VEScenarioViewer/data/VERPAT/category-cfg.js
@@ -0,0 +1,182 @@
+var catconfig = [
+  {
+    "NAME": ["Bicycles"],
+    "DESCRIPTION": ["Network improvements, incentives, and technologies that encourage bicycling and other light-weight vehicle travel."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["B"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["B"],
+            "LEVEL": ["2"]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "NAME": ["Demand Management"],
+    "DESCRIPTION": ["Programs to encourage less private vehicle travel."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["D"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["D"],
+            "LEVEL": ["2"]
+          }
+        ]
+      },
+      {
+        "NAME": ["3"],
+        "INPUTS": [
+          {
+            "NAME": ["D"],
+            "LEVEL": ["3"]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "NAME": ["Land Use"],
+    "DESCRIPTION": ["Distribution of population and employment by place type."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["L"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["L"],
+            "LEVEL": ["2"]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "NAME": ["Parking"],
+    "DESCRIPTION": ["Extent of and charges for priced parking."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["P"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["P"],
+            "LEVEL": ["2"]
+          }
+        ]
+      },
+      {
+        "NAME": ["3"],
+        "INPUTS": [
+          {
+            "NAME": ["P"],
+            "LEVEL": ["3"]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "NAME": ["Transit"],
+    "DESCRIPTION": ["Level of public transit service."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["T"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["T"],
+            "LEVEL": ["2"]
+          }
+        ]
+      },
+      {
+        "NAME": ["3"],
+        "INPUTS": [
+          {
+            "NAME": ["T"],
+            "LEVEL": ["3"]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "NAME": ["Vehicle Travel Cost"],
+    "DESCRIPTION": ["Combination of fuel prices and charges to pay for roadway costs and possibly externalities."],
+    "LEVELS": [
+      {
+        "NAME": ["1"],
+        "INPUTS": [
+          {
+            "NAME": ["C"],
+            "LEVEL": ["1"]
+          }
+        ]
+      },
+      {
+        "NAME": ["2"],
+        "INPUTS": [
+          {
+            "NAME": ["C"],
+            "LEVEL": ["2"]
+          }
+        ]
+      },
+      {
+        "NAME": ["3"],
+        "INPUTS": [
+          {
+            "NAME": ["C"],
+            "LEVEL": ["3"]
+          }
+        ]
+      }
+    ]
+  }
+];
