68a6bafe24b36fe2d9b325b0a976bbb071df35be
gregorbj <gregorbj@gmail.com>
2019-01-24 11:09:46 -0800

    Modify the test.R script to source the verspm_test.R script

--- sources/modules/VESimHouseholds/tests/scripts/test.R
+++ sources/modules/VESimHouseholds/tests/scripts/test.R
@@ -1,45 +1,6 @@
-library(visioneval)
-library(filesstrings)
+#test.R
+#------
+#Test script which calls scripts for testing modules of different models
 
-#Test CreateHouseholds.R module
-source("R/CreateHouseholds.R")
-testModule(
-  ModuleName = "CreateHouseholds",
-  LoadDatastore = FALSE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test PredictWorkers module
-source("R/PredictWorkers.R")
-testModule(
-  ModuleName = "PredictWorkers",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test AssignLifeCycle module
-source("R/AssignLifeCycle.R")
-testModule(
-  ModuleName = "AssignLifeCycle",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test PredictIncome module
-source("R/PredictIncome.R")
-testModule(
-  ModuleName = "PredictIncome",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Finish up
-setwd("tests")
-tar("Datastore.tar", "Datastore")
-dir.remove("Datastore")
-setwd("..")
-
+#Test for VERSPM
+source("tests/scripts/verspm_test.R")
