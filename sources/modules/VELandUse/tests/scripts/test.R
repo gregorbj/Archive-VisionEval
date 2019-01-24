fcafcecb34463073420f34174de3c83d9c176d41
gregorbj <gregorbj@gmail.com>
2019-01-24 11:25:01 -0800

    Create test.R script which sources the verspm_test.R and verpat_test.R scripts. Run tests.

---
+++ sources/modules/VELandUse/tests/scripts/test.R
@@ -0,0 +1,9 @@
+#test.R
+#------
+#Test script which calls scripts for testing modules of different models
+
+#Test for VERSPM
+source("tests/scripts/verspm_test.R")
+
+#Test for VERPAT
+source("tests/scripts/verpat_test.R")
