8ef58ec0974194b6a4d33f48e6f6118a412e1f6c
gregorbj <gregorbj@gmail.com>
2019-01-24 20:04:04 -0800

    Move module tests to new system operating off common datastore, inputs, defs. Run tests. Rebuild package.

--- sources/modules/VEPowertrainsAndFuels/tests/scripts/test.R
+++ sources/modules/VEPowertrainsAndFuels/tests/scripts/test.R
@@ -1,46 +1,7 @@
-library(visioneval)
-library(data.table)
-library(filesstrings)
+#test.R
+#------
+#Test script which calls scripts for testing modules of different models
 
-#Load datastore from VERoadPerformance package
-file.copy("../VEHouseholdTravel/tests/Datastore.tar", "tests/Datastore.tar", overwrite = TRUE)
-setwd("tests")
-untar("Datastore.tar")
-file.remove("Datastore.tar")
-setwd("..")
+#Test for VERSPM
+source("tests/scripts/verspm_test.R")
 
-#Load energy and emissions default data
-load("data/PowertrainFuelDefaults_ls.rda")
-
-#Test Initialize module
-source("R/Initialize.R")
-testModule(
-  ModuleName = "Initialize",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test CalculateCarbonIntensity module
-source("R/CalculateCarbonIntensity.R")
-testModule(
-  ModuleName = "CalculateCarbonIntensity",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Test AssignHhVehiclePowertrain module
-source("R/AssignHhVehiclePowertrain.R")
-testModule(
-  ModuleName = "AssignHhVehiclePowertrain",
-  LoadDatastore = TRUE,
-  SaveDatastore = TRUE,
-  DoRun = TRUE
-)
-
-#Finish up
-setwd("tests")
-tar("Datastore.tar", "Datastore")
-dir.remove("Datastore")
-setwd("..")
