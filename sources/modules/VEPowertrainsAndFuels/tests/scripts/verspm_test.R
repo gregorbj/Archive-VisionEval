7be6bb22660f3b4a3f325556d83effe65b4f7381
gregorbj <gregorbj@gmail.com>
2019-01-24 20:05:18 -0800

    Test script with SaveDatastore toggled to FALSE

---
+++ sources/modules/VEPowertrainsAndFuels/tests/scripts/verspm_test.R
@@ -0,0 +1,31 @@
+#verspm_test.R
+#-------------
+
+library(visioneval)
+library(data.table)
+library(filesstrings)
+source("tests/scripts/test_functions.R")
+
+#Define test setup parameters
+TestSetup_ls <- list(
+  TestDataRepo = "../Test_Data/VE-RSPM",
+  DatastoreName = "Datastore.tar",
+  LoadDatastore = TRUE,
+  TestDocsDir = "verspm",
+  ClearLogs = TRUE,
+  # SaveDatastore = TRUE
+  SaveDatastore = FALSE
+)
+
+#Define the module tests
+Tests_ls <- list(
+  Initialize = c(LoadDatastore = TRUE, SaveDatastore = TRUE, DoRun = TRUE),
+  CalculateCarbonIntensity = c(LoadDatastore = TRUE, SaveDatastore = TRUE, DoRun = TRUE),
+  AssignHhVehiclePowertrain = c(LoadDatastore = TRUE, SaveDatastore = TRUE, DoRun = TRUE)
+)
+
+#Set up, run tests, and save test results
+setUpTests(TestSetup_ls)
+doTests(Tests_ls, TestSetup_ls)
+saveTestResults(TestSetup_ls)
+
